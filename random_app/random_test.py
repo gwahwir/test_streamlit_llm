4643e364580c9fd00788a530ef62e808530329a0
Wilson <wilson.wants.an.email@gmail,com>
2023-08-28 21:36:02 +0800

    test "with"

--- random_app/random_test.py
+++ random_app/random_test.py
@@ -2,7 +2,17 @@ import streamlit as st
 
 st.title('🦜🔗 Test Trial App')
 
-st.sidebar.selectbox("How would you like to be contacted?", ("Email", "Home phone", "Mobile phone","snail mail"))
+
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
+
+    add_selectbox = st.selectbox(
+       "How would you like to be contacted?", 
+       ("Email", "Home phone", "Mobile phone","snail mail")
+       )
 
 with st.form('my_form'):
   text = st.text_area('Please enter your query:', 'What are the three key pieces of advice for learning how to code?')
