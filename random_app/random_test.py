fe35e9abd498eed81bfbe2c36a8b087f8f961c6d
Wilson <wilson.wants.an.email@gmail,com>
2023-08-28 21:37:27 +0800

    added slow for radio

--- random_app/random_test.py
+++ random_app/random_test.py
@@ -6,7 +6,7 @@ st.title('🦜🔗 Test Trial App')
 with st.sidebar:
     add_radio = st.radio(
         "Choose a shipping method",
-        ("Standard (5-15 days)", "Express (2-5 days)")
+        ("Standard (5-15 days)", "Express (2-5 days)", "SLOW (100 days)")
     )
 
     add_selectbox = st.selectbox(
