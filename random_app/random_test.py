6faeaf4f83038800d44f71480fb3c4ca4d172ff6
Wilson <wilson.wants.an.email@gmail,com>
2023-09-02 00:26:15 +0800

    test vid

--- random_app/random_test.py
+++ random_app/random_test.py
@@ -11,20 +11,17 @@ st.latex(r''' a+a r^1+a r^2+a r^3 ''')
 
 
 with st.sidebar:
-    
     add_radio = st.radio(
         "Choose a shipping method",
         ("Standard (5-15 days)", "Express (2-5 days)", "SLOW (100 days)")
     )
-
     add_selectbox = st.selectbox(
        "How would you like to be contacted?", 
        ("Email", "Home phone", "Mobile phone","snail mail")
        )
-    
     with st.echo():
         st.write("This code will be printed to the sidebar.")
-    
+
     st.write("where will this code be?")
 
     
@@ -39,3 +36,5 @@ with st.form('my_form'):
     with st.spinner("Loading..."):
         time.sleep(5)
     st.success("Done!")
+
+st.video("https://www.youtube.com/watch?v=iZTeva0WSTQ")
